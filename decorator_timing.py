52a1dd4b78d8adbc949edbf5020a001f2e20dd32
kmuhin <57116477+kmuhin@users.noreply.github.com>
2021-11-02 12:44:17 +0300

    КЕГЭ-2021

--- decorator_timing.py
+++ decorator_timing.py
@@ -2,13 +2,14 @@ import sys
 import time
 from functools import wraps
 
+
 def timing(f):
     @wraps(f)
     def wrap(*args, **kwargs):
         ts = time.perf_counter()
         result = f(*args, **kwargs)
         te = time.perf_counter()
-        print(f'func:{f.__name__}  took: {te-ts:10f}', file=sys.stderr)
+        print(f'{f.__module__}.{f.__name__}  took: {te - ts:10f}', file=sys.stderr)
         return result
-    return wrap
 
+    return wrap
